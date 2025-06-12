9a06e08dbc0ced37c6fc60a4f86987595c79fccd
Benson Williams <anthonybensonba837@gmail.com>
2025-06-12 16:11:27 -0500

    initial push

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 // 1. Alter the text color of the paragraph to a shade of aqua.
-
-green
-
+document.querySelector('p').style.color = 'aqua'
 // 2. Adjust the font size of the heading to 42 pixels.
-
-
+document.querySelector('heading').style.fontSize = '42px'
 // 3. Replace the text in the paragraph with the new Lorem Ipsum content.
-
+document.querySelector('p').innerText = 'nothing to see here.'
 // 4. Reduce the opacity of the 13th item to 30% transparency.
-
+document.querySelector('#item-13').style.opacity = '0.3'
 // 5. Modify the text of the 3rd item to read "Greetings!"
 
 // 6. Update the image below the list to display the new image.
@@ -24,3 +21,5 @@ green
 // 11. Assign the same class as the other list items to the new <li> element using JavaScript.
 
 // 12. Populate the new <li> with the text "Never gonna give you up."
+
+console.log('This Might Be A DOM Idea')
